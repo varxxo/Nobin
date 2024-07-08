6acbde7a138bcd1a943d75c7b810fce6fc6c29e5
vsmokshaa <vsmokshaa@gmail.com>
2024-07-08 17:47:49 +0530

    dom

--- ui/v2/data.js
+++ ui/v2/data.js
@@ -3,8 +3,20 @@ async function getData() {
   const url = "http://localhost:3000/dishes1";
 
   let data = await fetch(url)
-  console.log(await data.json())
+  let dishes = await data.json()
 
+  let container = document.querySelector('.cards');
+
+  for (let dish of dishes) {
+    let div = document.createElement('div');
+    div.className = 'card'
+    div.innerHTML = `
+      <img src="https://www.themanual.com/wp-content/uploads/sites/9/2021/05/how-to-make-the-perfect-milkshake.jpg?p=1">
+      <p>Chocolate Milkshake</p>
+      <p>A rich, creamy blend of smooth chocolate and cold milk, topped with whipped cream.</p>
+    `
+    container.append(div)
+  }
 
 }
 
