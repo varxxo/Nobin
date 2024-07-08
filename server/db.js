f0c94c1257da42396faa946964d3362ec167b734
varxxo <varshaaravi24@gmail.com>
2024-07-08 14:52:39 +0530

    db connection completed

--- server/db.js
+++ server/db.js
@@ -1,20 +1,21 @@
-const { Client } = require('pg')
-const connectionString = 'postgres://default:TH0iExPDJC8n@ep-long-snow-a4nm6blz.us-east-1.aws.neon.tech:5432/nobin?sslmode=require'
 
-async function run() {
-   const client = new Client({
-      connectionString
-    })
-    await client.connect()
-     
+const { Client } = require('pg');
+const connectionString = 'postgres://default:TH0iExPDJC8n@ep-long-snow-a4nm6blz.us-east-1.aws.neon.tech:5432/nobin?sslmode=require';
+
+exports.getFromDB = async function() {
+    const client = new Client({
+        connectionString
+    });
+    await client.connect();
+
     try {
-       const res = await client.query('SELECT * from dishes')
-       console.log(res.rows) // Hello world!
+        const res = await client.query('SELECT * from dishes');
+        console.log(res.rows);
+        return res.rows; 
     } catch (err) {
-       console.error(err);
+        console.error(err);
+        throw err; 
     } finally {
-       await client.end()
+        await client.end();
     }
-}
-
-run()
+};
