b1ab5c060f3d6b7c5185df089fa1c0d990d0462e
varxxo <varshaaravi24@gmail.com>
2024-07-09 13:39:53 +0530

    dishes fetched from db successfully

--- ui/v2/data.js
+++ ui/v2/data.js
@@ -2,22 +2,29 @@ async function getData() {
   console.log("get data")
   const url = "http://localhost:3000/dishes1";
 
-  let data = await fetch(url)
-  let dishes = await data.json()
+  try {
+    let response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    let dishes = await response.json();
 
-  let container = document.querySelector('.cards');
+    let container = document.querySelector('.cards');
+    container.innerHTML = ''; // Clear any existing content
 
-  for (let dish of dishes) {
-    let div = document.createElement('div');
-    div.className = 'card'
-    div.innerHTML = `
-      <img src="https://www.themanual.com/wp-content/uploads/sites/9/2021/05/how-to-make-the-perfect-milkshake.jpg?p=1">
-      <p>Chocolate Milkshake</p>
-      <p>A rich, creamy blend of smooth chocolate and cold milk, topped with whipped cream.</p>
-    `
-    container.append(div)
+    for (let dish of dishes) {
+      let div = document.createElement('div');
+      div.className = 'card';
+      div.innerHTML = `
+        <img src="${dish.url}" alt="${dish.name}">
+        <p>${dish.name}</p>
+        <p>${dish.descp}</p>
+      `;
+      container.append(div);
+    }
+  } catch (error) {
+    console.error("Failed to fetch data:", error);
   }
-
 }
 
 document.addEventListener('DOMContentLoaded', getData, false);
